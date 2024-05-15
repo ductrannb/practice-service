17dd606294eaad4c89fddd7a5f6aa3fc1afca15d
Tran Xuan Duc <xduc24122002@gmail.com>
2024-05-15 15:59:43 +0700

    change port

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.use((err, req, res, next) => {
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
