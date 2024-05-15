57ffab9ce30f53333a10f8da8df7ff22dd8503e4
Tran Xuan Duc <xduc24122002@gmail.com>
2024-05-15 16:07:53 +0700

    edit msg

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 
 // simple route
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello world' });
+    res.json({ message: 'Hello world from Practice Service' });
 });
 
 const mathjax = require('mathjax-node')
